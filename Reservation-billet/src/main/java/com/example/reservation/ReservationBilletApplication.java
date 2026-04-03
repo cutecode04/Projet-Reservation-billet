1667d850cf8622b69a1f0a4493b1ebfb99a1ea11
Berna30 <bernaomb@outlook.com>
2026-04-03 14:14:34 -0400

    Nouvelle version avec base de données

---
+++ Reservation-billet/src/main/java/com/example/reservation/ReservationBilletApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationBilletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationBilletApplication.class, args);
+	}
+
+}
